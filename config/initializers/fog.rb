74ddabb93e0378da6e0418cce4640d0a0b5f2433
erik <erikbatres@gmail.com>
2014-01-23 06:50:25 +0000

    Edit more code change due to silly mistakes.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config| 
   config.fog_credentials = { 
     :provider               => 'AWS', 
-    :aws_access_key_id      => ENV['AKIAJKRYRY35UG77T7IQ'], 
-    :aws_secret_access_key  => ENV['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'] 
+    :aws_access_key_id      => ENV['AWS-ACCESS_KEY_ID'], 
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] 
   } 
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = false 
