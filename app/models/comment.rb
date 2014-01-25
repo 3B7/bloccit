7f1e059f2e9f7d52cd1d04fffe2e21bef288ed1c
erik <erikbatres@gmail.com>
2014-01-25 21:34:05 +0000

    Revised code for Comments checkpoint

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :body, :post
 
-  validates :body, length {minimum: 5}, presence: true
-  validates :user, presence :true
+  validates :body, length: {minimum: 5}, presence: true
+  validates :user, presence: true
   
 end
