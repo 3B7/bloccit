40d607b35053bc522e12e0eccd22451f4d38e106
erik <erikbatres@gmail.com>
2014-01-26 01:08:03 +0000

    Destroyed topics, posts, and comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,18 @@ class CommentsController < ApplicationController
       flash[:notice] = "There was an error saving the comment. Please try again later."
       render 'posts/show'
     end
-
   end
-end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+    authorize! :destroy, @comment, message: = "You need to own the comment to delete it."
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic, @post]
+  end
