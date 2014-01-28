e12365e51769b0522983815a910cf953a2ab1184
erik <erikbatres@gmail.com>
2014-01-28 09:45:04 +0000

    partically complete voting checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,30 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title, :topic, :image
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
   
   default_scope order('created_at DESC')
 
-  mount_uploader :image
+  mount_uploader :image, ImageUploader
+
+  def up_votes
+    self.votes.where(value: 1).count
+  end
+
+  def down_votes
+    self.votes.where(value: -1).count
+  end
+
+  def points
+    self.votes.sum(:value).to_i
+  end
+
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
 end
