ed3367b4f00414b6842d18a73090a59ae5a8ad0d
erik <erikbatres@gmail.com>
2014-02-06 06:48:48 +0000

    Created show / hide jQuery function and others

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,42 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+
+
+$(document).ready(function() {
+  $(".js-show-hide").click(function() {
+    var selector = "." + $(this).attr('data-selector');
+    if ($(selector).is(":visible")) {
+      $(selector).slideUp();
+    }
+    else {
+      $(selector).slideDown();
+    }
+    return false;
+  });
+});
+
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $(".current-popular-users").fadeOut();
+  });
+});
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("<span>WHOA, RIGHT?!</span>").appendTo(".p1");
+  });
+});
+
+$(document).ready(function(){
+  $(".boton").click(function(){
+    $("<span>ASOMBROSO, NO?!</span>").appendTo(".p2");
+  });
+});
+
+$(document).ready(function(){
+  $(".red").dblclick(function(){
+    $(".p3").addClass("h6")
+  })
+})
